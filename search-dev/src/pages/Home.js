3bd25eefe48341263a687db90749a0f654c8244f
Luan Italo <luanitalo.dev@gmail.com>
2022-01-22 12:13:41 -0300

    Criando a primeira pagina

---
+++ search-dev/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Search from '../components/Search'
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <Search />
+      </div>
+    )
+  }
+}
+
+export default Home

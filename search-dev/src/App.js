6ccca6abd60dcf0054d0314a1924906319a483ac
Luan Italo <luanitalo.dev@gmail.com>
2022-01-25 08:33:16 -0300

    Axios configurado - API buscando dados do GitHub

--- search-dev/src/App.js
+++ search-dev/src/App.js
@@ -1,10 +1,25 @@
+import React, { useEffect, useState } from 'react'
 import './App.css'
 import Home from './pages/Home'
+import api from './services/api'
 
 function App() {
+  const [user, setUser] = useState()
+
+  useEffect(() => {
+    api
+      .get('/users/LuanItaloDev')
+      .then(response => setUser(response.data))
+      .catch(err => {
+        console.error('ops! ocorreu um erro' + err)
+      })
+  }, [])
+
   return (
     <div className="App">
       <Home />
+      <p>Usuário: {user?.login}</p>
+      <p>Biografia: {user?.bio}</p>
     </div>
   )
 }
